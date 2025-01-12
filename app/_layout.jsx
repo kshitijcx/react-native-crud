f9aed05f1f85c2ab9c6418ede491f7666bb3fcb8
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-01-12 17:58:48 +0530

    add render function for flatlist

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,10 +2,11 @@ import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return;
-  <SafeAreaProvider>
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
-  </SafeAreaProvider>;
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
-import { Text, View, StyleSheet, TextInput, Pressable } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  FlatList,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function Index() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { id: 1, title: "hello", completed: false },
+  ]);
   const [currTodo, setCurrTodo] = useState("");
+
   const addTodo = () => {
     if (currTodo.trim()) {
       const id = todos.length > 0 ? todos[0].id + 1 : 1;
@@ -31,8 +42,27 @@ export default function Index() {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const renderItem = ({ item }) => (
+    <View style={styles.todoItem}>
+      <Text
+        style={[styles.todoText, item.completed && styles.completedText]}
+        onPress={() => toggleTodo(item.id)}
+      >
+        {item.title}
+      </Text>
+      <Pressable onPress={() => removeTodo(item.id)}>
+        <MaterialCommunityIcons
+          name="delete-circle"
+          size={36}
+          color="red"
+          selectable={undefined}
+        />
+      </Pressable>
+    </View>
+  );
+
   return (
-    <SafeAreaView style={StyleSheet.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
@@ -45,6 +75,12 @@ export default function Index() {
           <Text style={styles.addButtonText}>Add</Text>
         </Pressable>
       </View>
+      <FlatList
+        data={todos}
+        renderItem={renderItem}
+        keyExtractor={(todo) => todo.id}
+        contentContainerStyle={{ flexGrow: 1 }}
+      />
     </SafeAreaView>
   );
 }
@@ -81,7 +117,7 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   addButtonText: {
-    fontSize:18,
-    color:'black',
+    fontSize: 18,
+    color: "black",
   },
 });
